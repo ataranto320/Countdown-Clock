7e20400c34d3e247c946a29513938ade6118a98e
Austin T <austin.taranto@gmail.com>
2020-03-31 11:51:12 -0400

    round the numbers

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ function timer(seconds) {
     const then = now + seconds * 1000;
     // console.log({now, then});
     setInterval(() => {
-        const secondsLeft = (then - Date.now()) / 1000;
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
         console.log(secondsLeft);
     }, 1000);
 }
