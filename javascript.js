ce16cde5c2fa8cb3be69ea9a24ffb1b0aaab9a53
Austin T <austin.taranto@gmail.com>
2020-03-31 13:27:49 -0400

    display end time function and countdown fix in timer function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
     // console.log({now, then});
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check if we should stop it
         if (secondsLeft < 0) {
@@ -27,4 +29,15 @@ function displayTimeLeft(seconds) {
     document.title = display;
     timerDisplay.textContent = display;
     // console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    // alternative
+    // const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? "0" : ""}${minutes}`;
+    //alternative
+    // endTime.textContent = `Be Back At ${adjustedHour}:${minutes}`;
 }
