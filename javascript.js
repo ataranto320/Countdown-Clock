a8f33766f2b76a76fb44d1a52a8250dce4928c22
Austin T <austin.taranto@gmail.com>
2020-03-31 11:57:05 -0400

    stopping at 0

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     // console.log({now, then});
     setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
         console.log(secondsLeft);
     }, 1000);
 }
